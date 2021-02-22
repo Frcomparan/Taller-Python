ac6e3528e9bc8ea30b1861e5ef7987304c00a071
Frcomparan <78762927+Frcomparan@users.noreply.github.com>
2021-02-21 19:54:12 -0600

    Delete loteria.py

--- Ejercicios_Propuestos_01/E_Datos_Compuestos/loteria.py
+++
@@ -1,8 +0,0 @@
-#Preguntar numeros de loteria al usuario
-
-numero_ganadores = []
-for i in range(6):
-    numero_ganadores.append(int(input("Ingresa el primer numero ganador: ")))
-
-numero_ganadores.sort()
-print(f"Los números ganadores son: {numero_ganadores}")
