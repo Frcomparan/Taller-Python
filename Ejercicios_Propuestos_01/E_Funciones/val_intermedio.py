d652659ba89f2b3e42d04f99fc4937eaf84f431f
Frcomparan <78762927+Frcomparan@users.noreply.github.com>
2021-02-21 19:56:07 -0600

    Delete val_intermedio.py

--- Ejercicios_Propuestos_01/E_Funciones/val_intermedio.py
+++
@@ -1,9 +0,0 @@
-#Calcular el valor intermedio entre dos numeros
-
-n1=-12
-n2=24
-
-def intermedio(a,b):
-    return (f"El punto intermedio entre ambos puntos es: {((a+b)/2)}")
-
-print(intermedio(n1,n2))
